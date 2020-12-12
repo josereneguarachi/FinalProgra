7afcf6306f0c6725349af390dcb7421d65b90e9c
Jose Rene Guarachi Pitiga <a2019113988@estudiantes.upsa.edu.bo>
2020-12-12 12:28:29 -0400

    realizacion del models

---
+++ Final/Final/Models/final.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Final.Models
+{
+    public class final
+    {
+        public string operacion(int numero)
+        {
+            string texto = "";
+            if (numero < 0)
+            {
+                texto = "ERROOOR";
+            }
+            if (numero == 0)
+            {
+                texto = "Realizado por Jose Rene Guarachi";
+            }
+            if (numero > 0)
+            {
+                texto = "https://image.freepik.com/vector-gratis/numeros-cartel-imagen_1639-6453.jpg";
+            }
+            return texto;
+        }
+    }
+}
